e50a263a5e4b228deedb280461d63fe2333c6cba
sm7urw <henrik@korslind.com>
2026-03-18 15:49:41 +0100

    Update

--- living_light.py
+++ living_light.py
@@ -1,6 +1,5 @@
 import time
 import random
-import _thread
 from machine import Pin, PWM, reset
 
 print("=== Levande Ljus ===\n")
@@ -9,6 +8,7 @@ print("=== Levande Ljus ===\n")
 RED_PIN = 3
 GREEN_PIN = 4
 BLUE_PIN = 5
+BUTTON_PIN = 6
 
 # Konfigurera PWM
 red_pwm = PWM(Pin(RED_PIN), freq=1000)
@@ -16,64 +16,47 @@ green_pwm = PWM(Pin(GREEN_PIN), freq=1000)
 blue_pwm = PWM(Pin(BLUE_PIN), freq=1000)
 
 # Knapp
-button_pin = 6  # GPIO 6
-button = Pin(button_pin, Pin.IN, Pin.PULL_UP)
+button = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_UP)
 
 print("LED initierad - Njut av det levande ljuset!")
 print("Tryck knappen (GPIO 6) för att starta om ESP32\n")
 
 def set_color(red, green, blue):
-    """Sätt LED-färg (0-255 för varje färg)"""
-    # Konvertera från 0-255 till 0-1023 och INVERTERA
     red_pwm.duty(1023 - int(red * 4.008))
     green_pwm.duty(1023 - int(green * 4.008))
     blue_pwm.duty(1023 - int(blue * 4.008))
 
 def set_warm_color(brightness):
-    """Sätt varm färg (röd + gul) med given ljusstyrka"""
     red = brightness
-    green = int(brightness * 0.6)  # Lite gul blandning
+    green = int(brightness * 0.6)
     blue = 0
-    
-    # Lägg till lite flakering för levande effekt
+
     flicker = random.randint(-10, 10)
-    
     red = max(0, min(255, red + flicker))
     green = max(0, min(255, int(green + flicker * 0.5)))
-    
+
     set_color(red, green, blue)
 
-def check_button():
-    """Övervaka knappen i en separat tråd"""
-    print("✓ Knappövervakning startad\n")
-    
-    while True:
-        if button.value() == 0:  # Knappen tryckt in (LOW)
-            print("\n KNAPP TRYCKT - Startar om ESP32...\n")
-            time.sleep(1)
-            reset()  # Starta om
-        time.sleep(0.05)
-
-# Starta knappövervakning i en separat tråd
-_thread.start_new_thread(check_button, ())
-
-# HUVUDLOOP - Levande ljus-effekt
+# HUVUDLOOP
 try:
     while True:
-        # Långsam pulsande andnings-effekt
-        # Öka ljusstyrka
+        # Kontrollera knappen
+        if button.value() == 0:
+            print("\n🔄 KNAPP TRYCKT - Startar om ESP32...\n")
+            time.sleep(1)
+            reset()
+
+        # Levande ljus-effekt
         for brightness in range(50, 256, 3):
             set_warm_color(brightness)
             time.sleep(0.03)
-        
-        # Minska ljusstyrka
+
         for brightness in range(255, 49, -3):
             set_warm_color(brightness)
             time.sleep(0.03)
-        
-        # Varierande pauslängd för naturlig känsla
+
         time.sleep(random.uniform(0.5, 1.5))
 
 except KeyboardInterrupt:
     print("\nLevande ljus avslutat")
-    set_color(0, 0, 0)  # Släck LED:n
+    set_color(0, 0, 0)
