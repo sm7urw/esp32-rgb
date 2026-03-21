e937f669bc645ab4d2a7af0b49da8503df9c38d6
sm7urw <henrik@korslind.com>
2026-03-21 21:25:05 +0100

    Update living_light.py with new implementation.

--- living_light.py
+++ living_light.py
@@ -1,29 +1,18 @@
 import time
-import threading
 
 # Configuration Constants
 PWM_FREQUENCY = 1000  # PWM frequency in Hertz
 DEBOUNCE_TIME = 200  # Debounce time in milliseconds
 
-# Thread-safe mechanism to handle concurrent access
-class ThreadSafe:
-    def __init__(self):
-        self.lock = threading.Lock()
-
-    def execute(self, function, *args, **kwargs):
-        with self.lock:
-            return function(*args, **kwargs)
-
-# Improved class with debounce and proper frequency management
+# Simple implementation for MicroPython
 class LivingLight:
     def __init__(self):
         self.is_on = False
-        self.thread_safe = ThreadSafe()
 
     def set_light(self, state):
         # Debouncing logic
         time.sleep(DEBOUNCE_TIME / 1000.0)  # Convert milliseconds to seconds
-        self.thread_safe.execute(self._set_light, state)
+        self._set_light(state)
 
     def _set_light(self, state):
         self.is_on = state
